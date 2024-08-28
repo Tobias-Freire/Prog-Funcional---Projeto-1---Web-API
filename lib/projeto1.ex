3e784154fd05b6e9bfe936f889297c6bde1e7926
tobiasfreire <tobiasfreire005@gmail.com>
2024-08-28 09:12:02 -0300

    Implementação da função getQuestions()
    A função getQuestions retorna um array de perguntas

--- lib/projeto1.ex
+++ lib/projeto1.ex
@@ -8,13 +8,21 @@ defmodule Projeto1 do
   def getQuestions() do
     HTTPoison.get(@url)
     |> process_response
+    |> filtra_questoes
   end
-  
+
   defp process_response({:ok, %HTTPoison.Response{status_code: 200, body: b}}) do
     {:ok, b}
   end
   defp process_response({:error, r}), do: {:erro, r}
   defp process_response({:ok, %HTTPoison.Response{status_code: _, body: b}}), do: {:error, b}
 
+  defp filtra_questoes({:error, _}), do: IO.puts("Erro!")
+  defp filtra_questoes({:ok, json}) do
+    {:ok, resp} = Poison.decode(json)
+    respMaps = resp["results"]
+    questoes = for map <- respMaps, do: map["question"]
+    questoes
+  end
 
 end
