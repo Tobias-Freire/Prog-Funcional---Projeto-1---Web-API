b9634d7eea9d1a1c35e3a70fbf6d600c47ff2ab3
Everton Llarena <evertonemanuel191@gmail.com>
2024-09-02 07:13:59 -0300

    Modifiquei o codigo para mostrar uma questao por vez

--- lib/QuestionsInfoGetter.ex
+++ lib/QuestionsInfoGetter.ex
@@ -10,20 +10,146 @@ defmodule QuestionsInfoGetter do
 
   @url "https://opentdb.com/api.php?amount="
 
-  def getQuestions() do
-    IO.puts("Digite a quantidade de questões que você quer:")
-    q = IO.gets("") |> String.trim() # Remove a nova linha do final
-    urlOpt = @url <> q
 
-    with {:ok, json} <- HTTPoison.get(urlOpt) |> process_response do
-      questoes = filtra_questoes({:ok, json})
-      opcoes = filtra_opcoes({:ok, json})
-      respostas = filtra_respostas({:ok, json})
-      [{:questoes, questoes}, {:opcoes, opcoes}, {:respostas, respostas}]
+  #definindo um mapa das categorias.
+  @categoria %{
+    "conhecimento gerais" => 9,
+    "livros" => 10,
+    "filmes" => 11,
+    "música" => 12,
+    "musicais e teatros" => 13,
+    "televisão" => 14,
+    "jogos digitais" => 15,
+    "jogos de mesa" => 16,
+    "ciência" => 17,
+    "computadores" => 18,
+    "matemática" => 19,
+    "mitologia" => 20,
+    "esportes" => 21,
+    "geografia" => 22,
+    "história" => 23,
+    "política" => 24,
+    "arte" => 25,
+    "celebridades" => 26,
+    "animais" => 27,
+    "veículos" => 28,
+    "quadrinhos" => 29,
+    "gadgets" => 30,
+    "anime e mangá" => 31,
+    "desenhos" => 32
+  }
+
+  @dificulade %{
+    "fácil" => "easy",
+    "médio" => "medium",
+    "difícil" => "hard"
+  }
+
+  @tipo %{
+    "múltipla" => "multiple",
+    "vouf" => "boolean"
+  }
+
+  #retorna o valor da chave passada.
+  def consultar_categoria(categ) do
+    categoria = Map.get(@categoria, categ)
+    Integer.to_string(categoria)#transforma em string, ja que o valor eh um numero
+  end
+
+  def consultar_dificuldade(difi) do
+    Map.get(@dificulade, difi)
+  end
+
+  def consultar_tipo(tip) do
+    Map.get(@tipo, tip)
+  end
+
+  def getQuestions() do
+
+    IO.puts("Você deseja que as questões sejam aleátorias? Se sim, digite s, se não, digite n: ")
+    resposta = IO.gets("") |> String.trim() # Remove a nova linha do final
+
+    #trata a resposta dada pelo usuario.
+    if resposta == "s" do
+      IO.puts("Digite a quantidade de questões que você deseja, sendo o limite de 50: ")
+      q = IO.gets("") |> String.trim()
+      urlAleat = @url <> q
+
+      with {:ok, json} <- HTTPoison.get(urlAleat) |> process_response do
+        questoes = filtra_questoes({:ok, json})
+        opcoes = filtra_opcoes({:ok, json})
+        respostas = filtra_respostas({:ok, json})
+        mostrar_questoes(questoes, opcoes, respostas)
+      else
+        {:error, _reason} ->
+          IO.puts("Erro ao obter questões.")
+          {:error, "Não foi possível obter as questões."}
+      end
     else
-      {:error, _reason} ->
-        IO.puts("Erro ao obter questões.")
-        {:error, "Não foi possível obter as questões."}
+      IO.puts("Digite a quantidade de questões que voce quer, sendo o limite de 50: ")
+      q = IO.gets("") |> String.trim()
+
+      IO.puts("Digite a categoria que você deseja, são elas: ")
+      Enum.each(@categoria, fn {numero, _cate} -> IO.puts "#{numero}" end)
+      categ = IO.gets("") |> String.trim()
+      c = consultar_categoria(categ)
+
+      IO.puts("Digite a dificuldade, são elas: ")
+      Enum.each(@dificulade, fn {original, _digitada} -> IO.puts "#{original}" end)
+      difi = IO.gets("") |> String.trim()
+      d = consultar_dificuldade(difi)
+
+      IO.puts("Digite o tipo, são eles: ")
+      Enum.each(@tipo, fn {original, _digitada} -> IO.puts "#{original}" end)
+      tip = IO.gets("") |> String.trim()
+      t = consultar_tipo(tip)
+
+       #s = string
+      sCategoria = "&category="
+      sDificuldade = "&dificulty="
+      sTipo = "&type="
+
+      urlCompleta = @url <> q <> sCategoria <> c <> sDificuldade <> d <> sTipo <> t
+      with {:ok, json} <- HTTPoison.get(urlCompleta) |> process_response do
+        questoes = filtra_questoes({:ok, json})
+        opcoes = filtra_opcoes({:ok, json})
+        respostas = filtra_respostas({:ok, json})
+        mostrar_questoes(questoes, opcoes, respostas)
+      else
+        {:error, _reason} ->
+          IO.puts("Erro ao obter questões.")
+          {:error, "Não foi possível obter as questões."}
+      end
+    end
+  end
+
+  #funcao para mostrar uma questao do trivia por vez
+  defp mostrar_questoes(questoes, opcoes, respostas) do
+    #usei o Enum.zip para combinar a lista de opcoes e repostas em uma lista de tuplas
+    #depois combinei essa lista de tuplas com a lista questoes usando Enum.zip novamente
+    #enum.each serve para iterar sobre cada tupla, para exibir uma questao por vez
+    #depois uso enum.each novamente com enum.with_index para que cada opcao de resposta tenha um indice
+    #as opcoes sao exibidas com seu indice respectivo
+    Enum.each Enum.zip(questoes, Enum.zip(opcoes, respostas)), fn {questao, {opcoes_questao, resposta_certa}} ->
+      IO.puts("Questão: #{questao}")#mostra a questao ao usuario
+
+      Enum.each(Enum.with_index(opcoes_questao), fn {opcao, index} ->
+        IO.puts("#{index + 1}. #{opcao}")
+      end)
+
+      #recebe a resposta do usuario, que necessariamente vai ser um numero
+      #esse numero vai servir para representar a escolha do usuario
+      resposta_usuario = IO.gets("Digite o número da sua resposta: ") |> String.trim()
+      resposta_usuario_index = String.to_integer(resposta_usuario) - 1
+
+      #verifica se a resposta dada pelo usuario combina com o indice da resposta certa
+      if Enum.at(opcoes_questao, resposta_usuario_index) == resposta_certa do
+        IO.puts("Correto!")
+      else
+        IO.puts("Incorreto! A resposta correta é: #{resposta_certa}")
+      end
+
+      IO.puts("") # Espaço em branco para melhor visualização
     end
   end
 
@@ -56,7 +182,7 @@ defmodule QuestionsInfoGetter do
     opcoes = for map <- respMaps do
       opcao_correta = map["correct_answer"]
       opcoes_incorretas = map["incorrect_answers"]
-      [opcao_correta | opcoes_incorretas]
+      [opcao_correta | opcoes_incorretas] |> Enum.shuffle() # Embaralha as opções
     end
     opcoes
   end
